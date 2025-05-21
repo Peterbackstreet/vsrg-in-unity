d4015fd0de4f5a1122f7b55f5beb37f854cdc2ca
Peterbackstreet <kyoukai1109@gmail.com>
2025-05-21 22:17:52 +0700

    note prefab

---
+++ Assets/Scripts/note.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class note : MonoBehaviour
+{
+  int type, lane;
+  float time, hold_duration;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
