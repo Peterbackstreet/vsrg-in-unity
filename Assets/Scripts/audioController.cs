372e003dcebf992d691aa44b87306e66c3ea7293
Peterbackstreet <kyoukai1109@gmail.com>
2025-05-22 03:36:50 +0700

    adding chart time and chart progress bar

--- Assets/Scripts/audioController.cs
+++ Assets/Scripts/audioController.cs
@@ -1,17 +1,40 @@
+using TMPro;
+using Unity.Collections;
+using UnityEditor.Timeline;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class audioController : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
+    [SerializeField] private TMP_Text chartTimeText;
+    [SerializeField] private Slider timeSlider;
+
+    private float chartLength, chartTime;
+    private string chartLengthText;
+
+    
     bool pause = true;
     void Start()
     {
         if (audioSource == null) audioSource = GetComponent<AudioSource>();
+        if (audioSource.clip != null) chartLength = audioSource.clip.length;
+        chartLengthText = FormatTime(chartLength);
     }
 
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) Pause();
+        displayChartTime();
+    }
+
+    void displayChartTime()
+    {
+        float curr_audioTime = audioSource.time;
+        chartTime = curr_audioTime;
+        chartTimeText.text = FormatTime(chartTime) + " / " + chartLengthText;
+        timeSlider.value = chartTime / chartLength;
+
     }
     public void Play()
     {
@@ -33,5 +56,11 @@ public class audioController : MonoBehaviour
     {
         if (audioSource != null) audioSource.Stop();
     }
-
+    private string FormatTime(float timeInSeconds)
+    {
+        int minutes = Mathf.FloorToInt(timeInSeconds / 60);
+        int seconds = Mathf.FloorToInt(timeInSeconds % 60);
+        int milisec = Mathf.FloorToInt(timeInSeconds*100 %100);
+        return string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milisec);
+    }
 }
